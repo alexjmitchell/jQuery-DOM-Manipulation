791cfe37c1b5134f0236ca376982410f45088c0f
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-10-15 23:26:23 -0700

    rewrote everything for the most part, worked on getting h3 to close when clicked or another h3 clicked

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
+// $(document).ready(function() {
+//   $("h3").on("click", function() {
+//     $(".content").removeClass("active")
+//     $(this)
+//       .find("+ .content")
+//       .addClass("active")
+//   })
+//   // if ($(".accordian div").hasClass("active")) {
+//   //   $("#block-1").show()
+//   // }
+// })
+
 $(document).ready(function() {
-  $("h3").on("click", function() {
-    $(".content").removeClass("active")
-    $(this)
-      .find("+ .content")
-      .addClass("active")
-  })
-  // if ($(".accordian div").hasClass("active")) {
-  //   $("#block-1").show()
-  // }
+  addListenerToH3Tags()
 })
+
+function addListenerToH3Tags() {
+  $("h3").on("click", function() {
+    handleClickEvent(this)
+  })
+}
+
+function toggleAccordion(element) {
+  $(element).toggleClass("background")
+  $(element)
+    .find("+ .content")
+    .toggleClass("active")
+}
+
+function closeOpenAccordions() {
+  $("h3").removeClass("background")
+  $(".content").removeClass("active")
+}
+
+function handleClickEvent(element) {
+  let activeCheck = $(element)
+    .find("+ .content")
+    .hasClass("active")
+
+  if (activeCheck) {
+    toggleAccordion(element)
+  } else {
+    closeOpenAccordions()
+    toggleAccordion(element)
+  }
+}
